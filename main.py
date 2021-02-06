a0fc87d7cef7eed6eb788d977383cf66204fbccb
GoldBigDragon <dnwndugod642@naver.com>
2021-02-07 08:00:13 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,96 @@
+import os
+import binascii
+import IIIlIlll
+import IIllIlll
+
+global IIIIIIIIII
+global IIIlIIIIII
+global IIIlIIlIII
+global IlIlIIlIII
+IIIIIIIIII = 0
+IIIlIIIIII = 0
+IIIlIIlIII = 0
+IlIlIIlIII = []
+
+def llllIIllll(IlllIIlIlI):
+  global IIIIIIIIII
+  global IIIlIIIIII
+  global IIIlIIlIII
+  global IlIlIIlIII
+  for IlIlIIlIlI in range(len(IlIlIIlIII)):
+    for IlllIIlIll in range(len(IlllIIlIlI)):
+      llllIIlIll = open(IlllIIlIlI[IlllIIlIll], 'rb')
+      IllllllllllI(IlIlIIlIII[IlIlIIlIlI], llllIIlIll)
+      llllIIlIll.close
+
+def llllllllll(lllllllllll):
+  global IIIIIIIIII
+  IIIIIIIIIIlI = IIIIIIIIIIll.read(512)
+  IIIIIIIIIIll.tell()
+  IIIIIIIIII+=512
+  return IIIIIIIIIIlI
+
+def IllllllllllI(IlIlIIlIII, f):
+  global IIIIIIIIII
+  global IIIlIIIIII
+  IIIIIIIIII = 0
+  IIlllIIlIllllllI=IlIlIIlIlI(f)
+  IlllIIlIllllll = str(binascii.b2a_hex(IIlllIIlIllllllI)[:len(IlIlIIlIII[1])])[1:]
+  if (IlIlIIlIII[1] in IlllIIlIllllll) :
+    IlllIIlIll=open("./Attack/"+str(IIIlIIIIII)+"."+IlIlIIlIII[0],"wb")
+    IlllIIlIll.write(IIlllIIlIllllllI)
+    while True:
+      IIlllIIlIllllllI = IlIlIIlIlI(f)
+      if (len(IIlllIIlIllllllI) == 0):
+        break
+      IlllIIlIll.write(IIlllIIlIllllllI)
+      if(IlIlIIlIII[2] != "NULL"):
+        IlllIIlIllllll = str(binascii.b2a_hex(IIlllIIlIllllllI)[:(len(IlIlIIlIII[2]) * -1)])[1:]
+        if (IlIlIIlIII[2] in IlllIIlIllllll):
+          break
+    IlllIIlIll.close()
+    IIIlIIIIII+=1
+
+def IIlllIIlIllll(IIlIIlllI):
+  IIllllll = []
+  for IIllllllI in range(3):
+    IIllllllI.append('IIIIlIll')
+  return IIllllll
+
+def IIlllIIlIlllllll(IIlIIlllI):
+  IIllllll = []
+  for IIllllllI in os.listdir(IIlIIlllI):
+    if os.path.isfile(IIlIIlllI+IIllllllI):
+      IIllllll.append(IIlIIlllI+IIllllllI)
+    elif (os.path.isdir(IIlIIlllI+IIllllllI) and IIllllllI != "Attack"):
+      IIllllll.extend(IIlllIIlIlllllll(IIlIIlllI+IIllllllI+"/"))
+  return IIllllll
+
+def IIIlIlllllll():
+  if os.path.isdir('Attack') :
+    pass
+  else :
+    os.mkdir("Attack")
+  global IlIlIIlIII
+  IlIlIIlIII = []
+  if os.path.exists("./config.txt") :
+    IIIlIlll = open("./config.txt", "r")
+    lines = tuple(IIIlIlll.readlines())
+    for IIIlIlllII in lines :
+      IIIlIlllIIl = IIIlIlllII.split("\n")[0].split(":")
+      if (len(IIIlIlllIIl) == 3):
+        IlIlIIlIII.append((IIIlIlllIIl[0], IIIlIlllIIl[1].replace(" ","").lower(), IIIlIlllIIl[2].replace(" ","").lower()))
+      elif (len(IIIlIlllIIl) == 2):
+        IlIlIIlIII.append((IIIlIlllIIl[0], IIIlIlllIIl[1].replace(" ","").lower(), "NULL"))
+    IIIlIlll.close()
+  else :
+    IIIlIlll = open("./config.txt", "w")
+    IIIlIlll.write("Attack:ffd8ff:ffd9\npng:89504e470d0a1a0a:49454e44ae426082");
+    IlIlIIlIII.append(("Attack", 'ffd8ff', 'ffd9'))
+    IlIlIIlIII.append(("Defense", '89504e470d0a1a0a', '49454e44ae426082'))
+    IIIlIlll.close()
+
+if __name__ == '__main__':
+  IIlllIIlIllll()
+  IIIlIlll.IIIlIlll()
+  IIllIlll.lIllIlll()

